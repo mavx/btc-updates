5a6d9acae8a0e4025d377c7088f688806366c1fb
mavx <auyongminhao@gmail.com>
2017-07-29 12:28:28 +0800

    Update endpoint. Add exception handling

--- update.py
+++ update.py
@@ -1,59 +1,83 @@
 import requests
 import psql
 
+ENDPOINT = {
+    'bitx': 'https://api.mybitx.com/api/1/ticker?pair=XBTMYR',
+    'bitav': 'https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD',
+    'coindesk': 'https://api.coindesk.com/v1/bpi/currentprice/USD.json',
+    'poloniex': 'https://poloniex.com/public?command=returnTicker'
+}
 
 def batch_call():
-    endpoint = {
-        'bitx': 'https://api.mybitx.com/api/1/ticker?pair=XBTMYR',
-        'bitav': 'https://apiv2.bitcoinaverage.com/ticker/USD',
-        'coindesk': 'https://api.coindesk.com/v1/bpi/currentprice/USD.json',
-        'poloniex': 'https://poloniex.com/public?command=returnTicker'
-    }
-
     results = {}
-    for key in endpoint:
-        r = requests.get(endpoint.get(key))
-        results[key] = r.json()
-    
+    for key in ENDPOINT:
+        try:
+            r = requests.get(ENDPOINT.get(key), timeout=200)
+            print('GET', r.url, r.status_code)
+            results[key] = r.json()
+        except Exception as e:
+            print(str(e))
+            pass
+
     return results
 
 
-def update_bitx(results):
-    db.execute(
-        "INSERT INTO bitx VALUES ((NOW()), (%s), (%s))"
-        , [results.get('ask'), results.get('bid')]
-        , commit=True
-    )
+def update_bitx(db, results):
+    try:
+        db.execute(
+            "INSERT INTO bitx VALUES ((NOW()), (%s), (%s))"
+            , [results.get('ask'), results.get('bid')]
+            , commit=True
+        )
+    except Exception as e:
+        print(str(e))
+        db.rollback()
 
-def update_bitav(results):
-    db.execute(
-        "INSERT INTO bitcoin_average VALUES ((NOW()), (%s), (%s))"
-        , [results.get('ask'), results.get('bid')]
-        , commit=True
-    )
+def update_bitav(db, results):
+    try:
+        db.execute(
+            "INSERT INTO bitcoin_average VALUES ((NOW()), (%s), (%s))"
+            , [results.get('ask'), results.get('bid')]
+            , commit=True
+        )
+    except Exception as e:
+        print(str(e))
+        db.rollback()
 
-def update_coindesk(results):
-    rate = results.get('bpi', {}).get('USD', {}).get('rate')
-    db.execute(
-        "INSERT INTO coindesk VALUES ((NOW()), (%s))"
-        , [float(rate.replace(',', ''))]
-        , commit=True
-    )
+def update_coindesk(db, results):
+    try:
+        rate = results.get('bpi', {}).get('USD', {}).get('rate')
+        db.execute(
+            "INSERT INTO coindesk VALUES ((NOW()), (%s))"
+            , [float(rate.replace(',', ''))]
+            , commit=True
+        )
+    except Exception as e:
+        print(str(e))
+        db.rollback()
 
-def update_poloniex(results):
-    rate = results.get('USDT_BTC', {})
-    db.execute(
-        "INSERT INTO poloniex VALUES ((NOW()), (%s), (%s))"
-        , [rate.get('lowestAsk'), rate.get('highestBid')]
-        , commit=True
-    )
+def update_poloniex(db, results):
+    try:
+        rate = results.get('USDT_BTC', {})
+        db.execute(
+            "INSERT INTO poloniex VALUES ((NOW()), (%s), (%s))"
+            , [rate.get('lowestAsk'), rate.get('highestBid')]
+            , commit=True
+        )
+    except Exception as e:
+        print(str(e))
+        db.rollback()
+        
+
+def main():
+    db = psql.Connection()
+    response = batch_call()
+    update_bitx(db, response.get('bitx', {}))
+    update_bitav(db, response.get('bitav', {}))
+    update_coindesk(db, response.get('coindesk', {}))
+    update_poloniex(db, response.get('poloniex', {}))
+    db.end()
 
 
 if __name__ == '__main__':
-    db = psql.Connection()
-    response = batch_call()
-    update_bitx(response.get('bitx'))
-    update_bitav(response.get('bitav'))
-    update_coindesk(response.get('coindesk'))
-    update_poloniex(response.get('poloniex'))
-    db.end()
+    main()
